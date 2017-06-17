8017e0f36f5d9566413777e310605a4e56bcb6e1
skji <jishankai@qq.com>
2017-06-17 17:38:15 +0800

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  root to: 'site#index'
+
+  get 'index', :to => 'visitors#index'
+  get 'visa',  :to => 'site#visa'
+  get 'insurance', :to => 'site#insurance'
+  get 'health', :to => 'site#health'
+  get 'whyus', :to => 'site#whyus'
 end
